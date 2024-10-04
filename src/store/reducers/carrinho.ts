02796314f5cabb6c74485cea642983d042dba168
Vinícius Silva <vinicius.silva_01@outlook.com>
2024-10-04 10:57:27 -0300

    usando Reduce Toolkit

---
+++ src/store/reducers/carrinho.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Produto } from '../../App'
+
+type CarrinhoState = {
+  itens: Produto[]
+}
+
+const initialState: CarrinhoState = {
+  itens: []
+}
+
+const carrinhoSlice = createSlice({
+  name: 'carrinho',
+  initialState,
+  reducers: {
+    adicionar: (state, action: PayloadAction<Produto>) => {
+      // Payload é uma ação que vem com esses dados adicionais (o payload). No Redux Toolkit, quando você define um reducer, se ele precisa de um valor extra para funcionar, esse valor extra é passado dentro de uma PayloadAction. / seria os itens
+      const produto = action.payload
+
+      if (state.itens.find((p) => p.id === produto.id)) {
+        alert('Item já adicionado')
+      } else {
+        state.itens.push(produto)
+      }
+    }
+  }
+})
+
+export const { adicionar } = carrinhoSlice.actions
+export default carrinhoSlice.reducer
